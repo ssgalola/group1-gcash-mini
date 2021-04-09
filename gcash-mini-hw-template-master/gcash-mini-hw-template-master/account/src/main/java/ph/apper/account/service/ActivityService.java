648b84ed8f79a73ec71e0b625f4c9ffa10a83e82
Mark Rhoy <markrhoy.sabal@mynt.xyz>
2021-04-09 19:57:04 +0800

    initialize activity service

---
+++ gcash-mini-hw-template-master/gcash-mini-hw-template-master/account/src/main/java/ph/apper/account/service/ActivityService.java
@@ -0,0 +1,10 @@
+package ph.apper.account.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActivityService {
+    public void recordActivity(){
+        //TODO..
+    }
+}
