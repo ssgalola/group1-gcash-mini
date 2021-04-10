f5669866f976ab4ebe5d50d890b4e55c00d9d686
johnashly.atienza <johnashly.atienza@eee.upd.edu.ph>
2021-04-10 21:19:16 +0800

    add generic response payload

---
+++ gcash-mini-hw-template-master/gcash-mini-hw-template-master/account/src/main/java/ph/apper/account/payload/GenericResponse.java
@@ -0,0 +1,12 @@
+package ph.apper.account.payload;
+
+import lombok.Data;
+
+@Data
+public class GenericResponse {
+    private String message;
+
+    public GenericResponse(String message){
+        this.message = message;
+    }
+}
