f9c9e6f0dc33222c463125be7211adfe8f597414
jratienza <johnashly.atienza@mynt.xyz>
2021-04-11 19:44:56 +0800

    use dynamic url mapping

--- gcash-mini-hw-template-master/gcash-mini-hw-template-master/account/src/main/java/ph/apper/account/util/ActivityService.java
+++ gcash-mini-hw-template-master/gcash-mini-hw-template-master/account/src/main/java/ph/apper/account/util/ActivityService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import ph.apper.account.App;
 import ph.apper.account.domain.Activity;
 
 @Service
@@ -13,13 +14,15 @@ public class ActivityService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ActivityService.class);
     @Autowired
     private final RestTemplate restTemplate;
+    private final App.GCashMiniProperties gCashMiniProperties;
 
-    public ActivityService(RestTemplate restTemplate) {
+    public ActivityService(RestTemplate restTemplate, App.GCashMiniProperties gCashMiniProperties) {
         this.restTemplate = restTemplate;
+        this.gCashMiniProperties = gCashMiniProperties;
     }
 
     public void postActivity(Activity activity){
-        ResponseEntity<Activity[]> activityResponse = restTemplate.postForEntity("http://localhost:8082",activity, Activity[].class);
+        ResponseEntity<Activity[]> activityResponse = restTemplate.postForEntity(gCashMiniProperties.getActivityUrl(),activity, Activity[].class);
         if (activityResponse.getStatusCode().is2xxSuccessful()) {
             LOGGER.info(activity.getAction() + " activity with" + activity.getIdentifier() + " identifier has been"  + " recorded.");
         }
