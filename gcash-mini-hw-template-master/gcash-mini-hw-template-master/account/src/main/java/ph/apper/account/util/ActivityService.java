dd86a71d7c6e2886bc2f3c6a0eba34e283bf6423
Mark Rhoy <markrhoy.sabal@mynt.xyz>
2021-04-11 20:12:43 +0800

    set activityService to fetch activity host in application properties

--- gcash-mini-hw-template-master/gcash-mini-hw-template-master/account/src/main/java/ph/apper/account/util/ActivityService.java
+++ gcash-mini-hw-template-master/gcash-mini-hw-template-master/account/src/main/java/ph/apper/account/util/ActivityService.java
@@ -3,10 +3,13 @@ package ph.apper.account.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ph.apper.account.domain.Activity;
+
+import java.util.Objects;
 
 @Service
 public class ActivityService {
@@ -14,12 +17,14 @@ public class ActivityService {
     @Autowired
     private final RestTemplate restTemplate;
 
+    private Environment env;
+
     public ActivityService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     public void postActivity(Activity activity){
-        ResponseEntity<Activity[]> activityResponse = restTemplate.postForEntity("http://localhost:8082",activity, Activity[].class);
+        ResponseEntity<Activity[]> activityResponse = restTemplate.postForEntity(Objects.requireNonNull(env.getProperty("activity.host")),activity, Activity[].class);
         if (activityResponse.getStatusCode().is2xxSuccessful()) {
             LOGGER.info(activity.getAction() + " activity with" + activity.getIdentifier() + " identifier has been"  + " recorded.");
         }
